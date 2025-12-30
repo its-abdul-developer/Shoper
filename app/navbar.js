322eafe404f092954a3729b2786f3543a794df3a
Abdul Wahab <abdulwahabcheema17@gmail.com>
2025-12-30 15:35:33 +0500

    navbar style completed

---
+++ app/navbar.js
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "./Components/Assets/logo.png";
+import cart_icon from "./Components/Assets/cart_icon.png";
+const Navbar = () => {
+
+  const [menu, setmenu] = useState("")
+
+  return (
+    <div>
+      <nav className="navbar flex justify-between items-center shadow-md px-14 py-2">
+        <div className="nav-logo flex items-center justify-center gap-2 font-medium">
+          <Image src={logo} alt="Shoper logo" width={40} height={40} />
+          <p>SHOPER</p>
+        </div>
+        <ul className="nav-menu flex gap-6 text-sm list-none">
+          <Link href="/"></Link>
+          <Link
+          onClick={()=>{
+            setmenu("shop")
+          }}
+          href="/shop">
+            <span>Shop 
+              {menu==="shop" ? <hr className="border-b-2 border-black " /> : null}
+            </span>
+          </Link>
+          <Link
+          onClick={()=>{
+            setmenu("men")
+          }}
+          href="/men">
+            <span>Men
+              {menu==="men" ? <hr className="border-b-2 border-black" /> : null}
+            </span>
+          </Link>
+          <Link
+          onClick={()=>{
+            setmenu("women")
+          }}
+          href="/women">
+            <span>Women
+              {menu==="women" ? <hr className="border-b-2 border-black" /> : null}
+            </span>
+          </Link>
+       
+          <Link
+          onClick={()=>{
+            setmenu("kids")
+          }}
+          href="/kids">
+            <span>Kids
+              {menu==="kids" ? <hr className="border-b-2 border-black" /> : null}
+            </span>
+          </Link>
+        </ul>
+        <div className="nav-login-cart flex gap-2 items-center justify-center text-sm">
+          <button className="px-7 py-1.5 border rounded-full active:bg-gray-200">
+            Login
+          </button>
+          <Image src={cart_icon} alt="Cart" width={20} height={20} />
+          <div className="nav-cart-count w-[15px] h-[15px] rounded-full bg-red-500 text-white flex items-center justify-center text-xs ml-[-10px] mt-[-17px]">
+            0
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
